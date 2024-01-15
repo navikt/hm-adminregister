93fb3b1d9c63efd02454b054d729308c4f10e610
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-01-15 09:33:40 +0100

    Dropdown for editering av delkontrakt

--- client/src/rammeavtaler/rammeavtale/DelkontrakterTab.tsx
+++ client/src/rammeavtaler/rammeavtale/DelkontrakterTab.tsx
@@ -1,19 +1,24 @@
-import { Alert, Button, ExpansionCard, Tabs, VStack } from '@navikt/ds-react'
+import { Alert, Button, Dropdown, ExpansionCard, HStack, Tabs, VStack } from '@navikt/ds-react'
 import { AgreementPostDTO } from '../../utils/response-types'
 import { useState } from 'react'
 import { Avstand } from '../../components/Avstand'
 import NewDelkontraktModal from './NewDelkontraktModal'
+import { MenuElipsisVerticalIcon, PlusCircleIcon } from '@navikt/aksel-icons'
 
-const DelkontrakterTab = ({ posts, agreementId, mutateAgreement }: { posts: AgreementPostDTO[], agreementId: string, mutateAgreement: () => void }) => {
+const DelkontrakterTab = ({ posts, agreementId, mutateAgreement }: {
+  posts: AgreementPostDTO[],
+  agreementId: string,
+  mutateAgreement: () => void
+}) => {
 
-  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [nyRammeavtaleModalIsOpen, setNyRammeavtaleModalIsOpen] = useState(false)
   const isFirstTime = posts.length === 0
 
   return (
     <>
       <NewDelkontraktModal
-        modalIsOpen={modalIsOpen}
-        setModalIsOpen={setModalIsOpen}
+        modalIsOpen={nyRammeavtaleModalIsOpen}
+        setModalIsOpen={setNyRammeavtaleModalIsOpen}
         oid={agreementId}
         mutateAgreement={mutateAgreement}
       />
@@ -24,18 +29,66 @@ const DelkontrakterTab = ({ posts, agreementId, mutateAgreement }: { posts: Agre
           </Alert>
         )}
         {!isFirstTime && (
-          <VStack gap='8'>
+          <VStack gap='5'>
             <>
               {posts.length > 0 && (
                 posts.map((post) => (
-                    <ExpansionCard key={post.nr} aria-label='default-demo'>
+                    <ExpansionCard size='small' key={post.nr} aria-label='default-demo'>
                       <ExpansionCard.Header>
-                        <ExpansionCard.Title>{post.title}</ExpansionCard.Title>
+                        <ExpansionCard.Title size='small'>{post.title}</ExpansionCard.Title>
                       </ExpansionCard.Header>
                       <ExpansionCard.Content>
-                        {post.description}
+                        <VStack>
+
+                          <p className='beskrivelse'><b>Beskrivelse:</b></p>
+                          {post.description}
+                          <HStack>
+                            <Button
+                              className='fit-content'
+                              variant='tertiary'
+                              icon={
+                                <PlusCircleIcon
+                                  title='Legg til produkt'
+                                  fontSize='1.5rem'
+                                />
+                              }
+                              onClick={() => {
+
+                              }}
+                            >
+                              <span className='produkt-button'>Legg til Produkt</span>
+                            </Button>
+                            <Dropdown>
+                              <Button
+                                variant='tertiary'
+                                icon={
+                                  <MenuElipsisVerticalIcon
+                                    title='Rediger'
+                                    fontSize='1.5rem'
+                                  />
+                                }
+                                style={{ marginLeft: 'auto' }}
+                                as={Dropdown.Toggle} />
+                              <Dropdown.Menu>
+                                <Dropdown.Menu.GroupedList>
+                                  <Dropdown.Menu.GroupedList.Item onClick={() => {
+                                  }}>
+                                    Endre tittel og beskrivelse
+                                  </Dropdown.Menu.GroupedList.Item>
+                                </Dropdown.Menu.GroupedList>
+                                <Dropdown.Menu.Divider />
+                                <Dropdown.Menu.List>
+                                  <Dropdown.Menu.List.Item href='https://nav.no'>
+                                    Slett
+                                  </Dropdown.Menu.List.Item>
+                                </Dropdown.Menu.List>
+                              </Dropdown.Menu>
+                            </Dropdown>
+                          </HStack>
+                        </VStack>
                       </ExpansionCard.Content>
                     </ExpansionCard>
+
                   ),
                 ))
               }
@@ -46,7 +99,7 @@ const DelkontrakterTab = ({ posts, agreementId, mutateAgreement }: { posts: Agre
         <Button
           className='fit-content'
           variant='secondary'
-          onClick={() => setModalIsOpen(true)}
+          onClick={() => setNyRammeavtaleModalIsOpen(true)}
         >
           Legg til delkontrakt
         </Button>
