78132c1c100f6dd3c7e5276185d8fa035cbf0a3a
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-01-23 20:39:19 +0100

    Tabellvisning av productAgreements og ekstern ingress

--- client/src/rammeavtaler/rammeavtale/delkontrakt/EditProductVariantsModal.tsx
+++ client/src/rammeavtaler/rammeavtale/delkontrakt/EditProductVariantsModal.tsx
@@ -1,7 +1,6 @@
-import { Button, HStack, Modal, Table, VStack } from '@navikt/ds-react'
-import React from 'react'
+import { Button, Checkbox, HStack, Modal, Table, VStack } from '@navikt/ds-react'
+import React, { useState } from 'react'
 import { ProductAgreementRegistrationDTOList } from '../../../utils/response-types'
-import { TrashIcon } from '@navikt/aksel-icons'
 
 interface Props {
   modalIsOpen: boolean
@@ -12,6 +11,13 @@ interface Props {
 
 const EditProducstVariantsModal = ({ modalIsOpen, setModalIsOpen, varianter }: Props) => {
 
+  const [selectedRows, setSelectedRows] = useState<string[]>([])
+  const toggleSelectedRow = (value: string) =>
+    setSelectedRows((list: string[]): string[] =>
+      list.includes(value)
+        ? list.filter((id: string) => id !== value)
+        : [...list, value],
+    )
 
   return (
     <Modal
@@ -28,45 +34,54 @@ const EditProducstVariantsModal = ({ modalIsOpen, setModalIsOpen, varianter }: P
         <div className='delkontrakter-tab__new-delkontrakt-container'>
           {varianter.length > 0 && (
             <VStack gap='2'>
-              <Table>
-                <Table.Header>
-                  <Table.Row>
-                    <Table.HeaderCell scope='col'>Navn på variant</Table.HeaderCell>
-                    <Table.HeaderCell scope='col'>Hms-nummer.</Table.HeaderCell>
-                    <Table.HeaderCell scope='col'>Lev-artnr.</Table.HeaderCell>
-                    <Table.HeaderCell scope='col'></Table.HeaderCell>
-                  </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                  {varianter.map((variant, i) => {
-                    return (
-                      <Table.Row key={i} shadeOnHover={false}>
-                        <Table.DataCell>{variant.articleName}</Table.DataCell>
-                        <Table.DataCell>{variant.hmsArtNr}</Table.DataCell>
-                        <Table.DataCell>{variant.supplierRef}</Table.DataCell>
-                        <Table.DataCell>
-                          <Button
-                            className='button-icon'
-                            iconPosition='right'
-                            variant={'tertiary'}
-                            icon={
-                              <TrashIcon
-                                title='Slett'
-                                fontSize='1.5rem'
-                              />
-                            }
+                <Table>
+                  <Table.Header>
+                    <Table.Row>
+                      <Table.HeaderCell scope='col'>Navn</Table.HeaderCell>
+                      <Table.HeaderCell scope='col'>HMS-nummer</Table.HeaderCell>
+                      <Table.HeaderCell scope='col'>Lev-artnr.</Table.HeaderCell>
+                      <Table.DataCell>
+                        <Checkbox
+                          checked={selectedRows.length === varianter.length}
+                          onChange={() => {
+                            selectedRows.length
+                              ? setSelectedRows([])
+                              : setSelectedRows(varianter.map(({ id }) => id!!))
+                          }}
+                          hideLabel
+                        >
+                          Velg alle rader
+                        </Checkbox>
+                      </Table.DataCell>
+                    </Table.Row>
+                  </Table.Header>
+                  <Table.Body>
 
-                            onClick={() => {
-
-                            }} />
-                        </Table.DataCell>
-                      </Table.Row>
-                    )
-                  })}
-                </Table.Body>
-              </Table>
+                    {varianter.map((variant, i) => {
+                      return (
+                        <Table.Row key={variant.id}>
+                          <Table.DataCell>{variant.articleName}</Table.DataCell>
+                          <Table.DataCell>{variant.hmsArtNr}</Table.DataCell>
+                          <Table.DataCell>{variant.supplierRef}</Table.DataCell>
+                          <Table.DataCell>
+                            <Checkbox
+                              hideLabel
+                              checked={selectedRows.includes(variant.id!!)}
+                              onChange={() => {
+                                toggleSelectedRow(variant.id!!)
+                              }}
+                              aria-labelledby={`id-${variant.id}`}
+                            >
+                              {' '}
+                            </Checkbox>
+                          </Table.DataCell>
+                        </Table.Row>
+                      )
+                    })}
+                  </Table.Body>
+                </Table>
             </VStack>
-          )}
+            )}
         </div>
       </Modal.Body>
       <Modal.Footer>
@@ -83,7 +98,7 @@ const EditProducstVariantsModal = ({ modalIsOpen, setModalIsOpen, varianter }: P
         </HStack>
       </Modal.Footer>
     </Modal>
-  )
+)
 }
 
 export default EditProducstVariantsModal
