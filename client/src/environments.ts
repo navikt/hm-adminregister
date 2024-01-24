343527b3ab31aca1789cf2dbc00f0d0c65c0ec1d
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-01-24 10:03:27 +0100

    ekstern not external

--- client/src/environments.ts
+++ client/src/environments.ts
@@ -1,14 +1,12 @@
 export const HM_REGISTER_URL = () => {
-  console.log(window.location.hostname)
-  console.log(window.location.origin)
-  if (window.location.hostname.includes('external')) {
+  if (window.location.hostname.includes('ekstern')) {
     return import.meta.env.VITE_HM_REGISTER_EXTERNAL_URL
   } else {
     return import.meta.env.VITE_HM_REGISTER_URL
   }
 }
 export const IMAGE_PROXY_URL = () => {
-  if (window.location.hostname.includes('external')) {
+  if (window.location.hostname.includes('ekstern')) {
     return import.meta.env.VITE_IMAGE_PROXY_EXTERNAL_URL
   } else {
     return import.meta.env.VITE_IMAGE_PROXY_URL
