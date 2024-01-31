2680a6f15b3c37fd0b25bc61bd8ded4702b5b2fd
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-01-31 14:34:42 +0100

    Fiksa compilefeil

--- client/src/error/ErrorFallback.tsx
+++ client/src/error/ErrorFallback.tsx
@@ -6,10 +6,10 @@ import { Error401Page } from './Error401Page'
 export const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
 
   if (error.status === 500) {
-    return <Error500Page />
+    return <Error500Page error={error} resetErrorBoundary={resetErrorBoundary} />
   } else if (error.status === 401) {
     return <Error401Page error={error} resetErrorBoundary={resetErrorBoundary} />
   }
 
-  return <Error500Page />
+  return <Error500Page error={error} resetErrorBoundary={resetErrorBoundary} />
 }
