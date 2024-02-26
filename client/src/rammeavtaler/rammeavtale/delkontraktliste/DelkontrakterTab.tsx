d3695ff1c1a45af069bcc72a666dfb0f480a7648
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-02-26 13:25:20 +0100

    Sortering av delkontrakt mvp

--- client/src/rammeavtaler/rammeavtale/delkontraktliste/DelkontrakterTab.tsx
+++ client/src/rammeavtaler/rammeavtale/delkontraktliste/DelkontrakterTab.tsx
@@ -5,8 +5,9 @@ import { Avstand } from "felleskomponenter/Avstand";
 import NewDelkontraktModal from "./NewDelkontraktModal";
 import { useProductVariantsByAgreementId } from "utils/swr-hooks";
 import { Delkontrakt } from "../delkontraktdetaljer/Delkontrakt";
-import { ChevronDownIcon, ChevronUpIcon } from "@navikt/aksel-icons";
+import { ArrowsUpDownIcon, ChevronDownIcon, ChevronUpIcon } from "@navikt/aksel-icons";
 import { reorderPosts } from "api/AgreementApi";
+import styled from "styled-components";
 
 const DelkontrakterTab = ({
   posts,
@@ -22,9 +23,7 @@ const DelkontrakterTab = ({
       .then((r) => {
         mutateAgreement();
       })
-      .catch((e) => {
-        console.log(e);
-      });
+      .catch((_) => {});
   };
 
   const {
@@ -37,7 +36,7 @@ const DelkontrakterTab = ({
 
   if (delkontrakterIsLoading)
     return (
-      <Tabs.Panel value="delkontrakter" className="tab-panel">
+      <Tabs.Panel value="delkontrakter">
         <Loader size="large" />
       </Tabs.Panel>
     );
@@ -52,56 +51,71 @@ const DelkontrakterTab = ({
         mutateDelkontrakter={mutateDelkontrakter}
       />
 
-      <Tabs.Panel value="delkontrakter" className="tab-panel">
+      <DelkontrakterTabsPanel value="delkontrakter">
         {isFirstTime && <Alert variant="info">Rammeavtalen trenger delkontrakter.</Alert>}
         {!isFirstTime && (
-          <VStack gap="5">
-            {posts.length > 0 &&
-              posts.map((post, i) => (
-                <HGrid columns="auto 60px" key={i}>
-                  <Delkontrakt
-                    key={i}
-                    delkontrakt={post}
-                    produkter={delkontrakter?.filter((produkt) => produkt.delkontraktNr === post.nr) || []}
-                    agreementId={agreementId}
-                    mutateDelkontrakter={mutateDelkontrakter}
-                    mutateAgreement={mutateAgreement}
-                  />
-                  <VStack gap="1" style={{ alignItems: "center" }}>
-                    {i !== 0 && (
-                      <Button
-                        aria-label="sorter-opp"
-                        size={"small"}
-                        variant="tertiary"
-                        icon={<ChevronUpIcon />}
-                        onClick={() => {
-                          reorderDelkontrakt(post.nr, posts[i - 1].nr);
-                        }}
-                      />
-                    )}
-                    {i !== posts.length - 1 && (
-                      <Button
-                        aria-label="sorter-ned"
-                        size={"small"}
-                        variant="tertiary"
-                        icon={<ChevronDownIcon />}
-                        onClick={() => {
-                          reorderDelkontrakt(post.nr, posts[i + 1].nr);
-                        }}
-                      />
-                    )}
-                  </VStack>
-                </HGrid>
-              ))}
-          </VStack>
+          <>
+            <Avstand marginTop={3} />
+            <HGrid columns="auto 60px" gap="5">
+              <div />
+              <div style={{ margin: "auto" }}>
+                <ArrowsUpDownIcon aria-hidden={true} fontSize="1.5rem" />
+              </div>
+            </HGrid>
+            <Avstand marginBottom={3} />
+            <HGrid columns="auto 60px" gap="4">
+              {posts.length > 0 &&
+                posts.map((post, i) => (
+                  <>
+                    <Delkontrakt
+                      key={i}
+                      delkontrakt={post}
+                      produkter={delkontrakter?.filter((produkt) => produkt.delkontraktNr === post.nr) || []}
+                      agreementId={agreementId}
+                      mutateDelkontrakter={mutateDelkontrakter}
+                      mutateAgreement={mutateAgreement}
+                    />
+                    <VStack gap="1" style={{ alignItems: "center" }}>
+                      {i !== 0 && (
+                        <Button
+                          aria-label="sorter-opp"
+                          size={"small"}
+                          variant="tertiary"
+                          icon={<ChevronUpIcon />}
+                          onClick={() => {
+                            reorderDelkontrakt(post.nr, posts[i - 1].nr);
+                          }}
+                        />
+                      )}
+                      {i !== posts.length - 1 && (
+                        <Button
+                          aria-label="sorter-ned"
+                          size={"small"}
+                          variant="tertiary"
+                          icon={<ChevronDownIcon />}
+                          onClick={() => {
+                            reorderDelkontrakt(post.nr, posts[i + 1].nr);
+                          }}
+                        />
+                      )}
+                    </VStack>
+                  </>
+                ))}
+            </HGrid>
+          </>
         )}
         <Avstand marginBottom={5} />
         <Button className="fit-content" variant="secondary" onClick={() => setNyRammeavtaleModalIsOpen(true)}>
           Legg til delkontrakt
         </Button>
-      </Tabs.Panel>
+      </DelkontrakterTabsPanel>
     </>
   );
 };
 
 export default DelkontrakterTab;
+
+export const DelkontrakterTabsPanel = styled(Tabs.Panel)`
+  margin-top: 0.5rem;
+  margin-bottom: 1rem;
+`;
