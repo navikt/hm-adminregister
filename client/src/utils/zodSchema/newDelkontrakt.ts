7894c85320162b23339bd54eaf8b112080c8f45c
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-03-14 15:04:57 +0100

    Validerer at ny delkontrakttittel har korrekt format

--- client/src/utils/zodSchema/newDelkontrakt.ts
+++ client/src/utils/zodSchema/newDelkontrakt.ts
@@ -1,6 +1,9 @@
 import z from "zod";
 
 export const createNewDelkontraktSchema = z.object({
-  tittel: z.string().min(1, { message: "Tittel på delkontrakt kan ikke være tom" }),
+  tittel: z
+    .string()
+    .min(1, { message: "Tittel på delkontrakt kan ikke være tom" })
+    .regex(RegExp("^[0-9]{1,2}[:.].*"), { message: "Tittel må starte med et tall etterfulgt av :  eller ." }),
   beskrivelse: z.string().min(1, { message: "Beskrivelse kan ikke være tom" }),
 });
