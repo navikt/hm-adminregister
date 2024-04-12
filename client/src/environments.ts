84007b231c98411a3a8774bab77f09396cb44b06
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-04-12 12:38:46 +0200

    Use urls from settings.js

--- client/src/environments.ts
+++ client/src/environments.ts
@@ -1,17 +1,14 @@
 export const HM_REGISTER_URL = () => {
   if (window.location.hostname.includes("ekstern")) {
-    return import.meta.env.VITE_HM_REGISTER_EXTERNAL_URL;
+    return window.appSettings.VITE_HM_REGISTER_EXTERNAL_URL;
   } else {
-    const fraSettings = window.appSettings.VITE_HM_REGISTER_URL;
-    console.log("fraSettings", fraSettings);
-    console.log("appSettings", window.appSettings);
-    return import.meta.env.VITE_HM_REGISTER_URL;
+    return window.appSettings.VITE_HM_REGISTER_URL;
   }
 };
 export const IMAGE_PROXY_URL = () => {
   if (window.location.hostname.includes("ekstern")) {
-    return import.meta.env.VITE_IMAGE_PROXY_EXTERNAL_URL;
+    return window.appSettings.VITE_IMAGE_PROXY_EXTERNAL_URL;
   } else {
-    return import.meta.env.VITE_IMAGE_PROXY_URL;
+    return window.appSettings.VITE_IMAGE_PROXY_URL;
   }
 };
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -22,6 +22,8 @@ export const routes = {
         const appSettings = {
           VITE_HM_REGISTER_URL: process.env.VITE_HM_REGISTER_URL,
           VITE_IMAGE_PROXY_URL: process.env.VITE_IMAGE_PROXY_URL,
+          VITE_HM_REGISTER_EXTERNAL_URL: process.env.VITE_HM_REGISTER_EXTERNAL_URL,
+          VITE_IMAGE_PROXY_EXTERNAL_URL: process.env.VITE_IMAGE_PROXY_EXTERNAL_URL,
           USE_MSW: process.env.USE_MSW === "true",
           MILJO: process.env.NAIS_CLUSTER_NAME,
         };
