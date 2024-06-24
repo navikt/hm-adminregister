d2e39f2be99ec441a31f8788f846185ba481aa3d
albert bayazidi <albert.bayazidi@gmail.com>
2024-06-24 14:37:29 +0200

    Adding news to navbar

--- client/src/news/News.tsx
+++ client/src/news/News.tsx
@@ -1,12 +1,26 @@
+import {Button, Heading} from "@navikt/ds-react";
+import {PlusIcon} from "@navikt/aksel-icons";
 
 
 const News = () => {
 
     return(
         <main className="show-menu">
-             HELLO WORLD
+          <div className="page__background-container">
+                 <Heading level="1" size="large" spacing>
+                   Nyheter
+                 </Heading>
+              <Button
+                  variant="secondary"
+                  size="medium"
+                  icon={<PlusIcon aria-hidden />}
+                  iconPosition="left"
+              >
+                  Opprett ny nyhetsmelding
+              </Button>
 
 
+          </div>
         </main>
     )
 }
