9879ec0f2f1f7c4302a4554460ca6008b51f9133
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-06-27 09:25:41 +0200

    Small fixes

--- client/src/api/VersionApi.ts
+++ client/src/api/VersionApi.ts
@@ -5,7 +5,7 @@ export const getDifferenceFromPublishedSeries = async (seriesUUID: string, versi
   return await fetchAPI(getPath(true, `/api/v1/series/versions/${seriesUUID}/compare/${version}/approved`), "GET");
 };
 
-export const getDifferenceFromPublishedVariant = async (productId: string, version: number): Promise<DifferenceDTO> => {
+const getDifferenceFromPublishedVariant = async (productId: string, version: number): Promise<DifferenceDTO> => {
   return await fetchAPI(getPath(true, `/api/v1/product/versions/${productId}/compare/${version}/approved`), "GET");
 };
 
--- client/src/produkter/diff/SeriesDiff.tsx
+++ client/src/produkter/diff/SeriesDiff.tsx
@@ -7,87 +7,84 @@ import { Strikethrough } from "produkter/diff/Strikethrough";
 export const SeriesDiff = ({ seriesDiff }: { seriesDiff: DifferenceDTO }) => {
   const { t } = useTranslation();
 
-  if (!seriesDiff) {
+  if (!seriesDiff || seriesDiff.status === "NO_DIFF") {
     return <BodyShort>Ingen endringer på serie</BodyShort>;
   }
-  if (seriesDiff.status === "NO_DIFF") {
-    return <BodyShort>Ingen endringer på serie</BodyShort>;
-  } else {
-    return (
-      <VStack gap="2">
-        <Heading size="xsmall">Endringer i serie</Heading>
-        {Object.entries(seriesDiff.diff.entriesDiffering).length > 0 && (
-          <VStack gap="3">
-            {Object.entries(seriesDiff.diff.entriesDiffering).map(
-              ([key, value], index) =>
-                key !== "seriesData.media" && (
-                  <VStack gap="2" key={index} className={styles.changeRow}>
-                    <BodyShort weight="semibold">{t(key)}</BodyShort>
-                    <Box>
-                      <VStack gap="1">
-                        <Box padding="2" background="surface-danger-subtle" className={styles.previous}>
-                          <Strikethrough>{arrayCheck(value.second)}</Strikethrough>
-                        </Box>
-                        <Box padding="2" background="surface-success-subtle" className={styles.current}>
-                          {arrayCheck(value.first)}
-                        </Box>
-                      </VStack>
-                    </Box>
-                  </VStack>
-                ),
-            )}
+
+  return (
+    <VStack gap="2">
+      <Heading size="xsmall">Endringer i serie</Heading>
+      {Object.entries(seriesDiff.diff.entriesDiffering).length > 0 && (
+        <VStack gap="3">
+          {Object.entries(seriesDiff.diff.entriesDiffering).map(
+            ([key, value], index) =>
+              key !== "seriesData.media" && (
+                <VStack gap="2" key={index} className={styles.changeRow}>
+                  <BodyShort weight="semibold">{t(key)}</BodyShort>
+                  <Box>
+                    <VStack gap="1">
+                      <Box padding="2" background="surface-danger-subtle" className={styles.previous}>
+                        <Strikethrough>{arrayCheck(value.second)}</Strikethrough>
+                      </Box>
+                      <Box padding="2" background="surface-success-subtle" className={styles.current}>
+                        {arrayCheck(value.first)}
+                      </Box>
+                    </VStack>
+                  </Box>
+                </VStack>
+              ),
+          )}
+        </VStack>
+      )}
+      {Object.entries(seriesDiff.diff.entriesOnlyOnLeft).length > 0 && (
+        <VStack gap="3">
+          <BodyShort>Nye verdier: </BodyShort>
+          {Object.entries(seriesDiff.diff.entriesOnlyOnLeft).map(
+            ([key, value], index) =>
+              key !== "seriesData.media" && (
+                <VStack gap="2" key={index} className={styles.changeRow}>
+                  <BodyShort weight="semibold">{t(key)}</BodyShort>
+                  <Box>
+                    <VStack gap="1">
+                      <Box padding="2" background="surface-success-subtle" className={styles.current}>
+                        {arrayCheck(value)}
+                      </Box>
+                    </VStack>
+                  </Box>
+                </VStack>
+              ),
+          )}
+        </VStack>
+      )}
+      {Object.entries(seriesDiff.diff.entriesOnlyOnRight).length > 0 && (
+        <VStack gap="3">
+          <BodyShort>Slettede verdier: </BodyShort>
+          {Object.entries(seriesDiff.diff.entriesOnlyOnRight).map(
+            ([key, value], index) =>
+              key !== "seriesData.media" && (
+                <VStack gap="2" key={index} className={styles.changeRow}>
+                  <BodyShort weight="semibold">{t(key)}</BodyShort>
+                  <Box>
+                    <VStack gap="1">
+                      <Box padding="2" background="surface-danger-subtle" className={styles.current}>
+                        <Strikethrough>{value as string}</Strikethrough>
+                      </Box>
+                    </VStack>
+                  </Box>
+                </VStack>
+              ),
+          )}
+        </VStack>
+      )}
+      {Object.entries(seriesDiff.diff.entriesDiffering).find(([key]) => key === "seriesData.media") && (
+        <Box>
+          <VStack gap="1">
+            <div className={styles.changeRow}>Det finnes endringer i bilde, dokumenter og/eller videoer.</div>
           </VStack>
-        )}
-        {Object.entries(seriesDiff.diff.entriesOnlyOnLeft).length > 0 && (
-          <VStack gap="3">
-            <BodyShort>Nye verdier: </BodyShort>
-            {Object.entries(seriesDiff.diff.entriesOnlyOnLeft).map(
-              ([key, value], index) =>
-                key !== "seriesData.media" && (
-                  <VStack gap="2" key={index} className={styles.changeRow}>
-                    <BodyShort weight="semibold">{t(key)}</BodyShort>
-                    <Box>
-                      <VStack gap="1">
-                        <Box padding="2" background="surface-success-subtle" className={styles.current}>
-                          {arrayCheck(value)}
-                        </Box>
-                      </VStack>
-                    </Box>
-                  </VStack>
-                ),
-            )}
-          </VStack>
-        )}
-        {Object.entries(seriesDiff.diff.entriesOnlyOnRight).length > 0 && (
-          <VStack gap="3">
-            <BodyShort>Slettede verdier: </BodyShort>
-            {Object.entries(seriesDiff.diff.entriesOnlyOnRight).map(
-              ([key, value], index) =>
-                key !== "seriesData.media" && (
-                  <VStack gap="2" key={index} className={styles.changeRow}>
-                    <BodyShort weight="semibold">{t(key)}</BodyShort>
-                    <Box>
-                      <VStack gap="1">
-                        <Box padding="2" background="surface-danger-subtle" className={styles.current}>
-                          <Strikethrough>{value as string}</Strikethrough>
-                        </Box>
-                      </VStack>
-                    </Box>
-                  </VStack>
-                ),
-            )}
-          </VStack>
-        )}
-        {Object.entries(seriesDiff.diff.entriesDiffering).find(([key]) => key === "seriesData.media") && (
-          <Box>
-            <VStack gap="1">
-              <div className={styles.changeRow}>Det finnes endringer i bilde, dokumenter og/eller videoer.</div>
-            </VStack>
-          </Box>
-        )}
-      </VStack>
-    );
-  }
+        </Box>
+      )}
+    </VStack>
+  );
 };
 
 const arrayCheck = (value: unknown) => {
