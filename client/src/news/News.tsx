e0678bbc5e5c553f8a37de835352e8e721c15e1c
albert bayazidi <albert.bayazidi@gmail.com>
2024-07-08 09:58:42 +0200

    added the ability to tab out of text editor

--- client/src/news/News.tsx
+++ client/src/news/News.tsx
@@ -1,10 +1,12 @@
-import { Alert, Heading, HGrid, HStack, Loader, ToggleGroup, VStack } from "@navikt/ds-react";
+import { Alert, Button, Heading, HGrid, HStack, Loader, ToggleGroup, VStack } from "@navikt/ds-react";
 import { useNavigate } from "react-router-dom";
 import { getPageNews } from "api/NewsApi";
 import React, { useState } from "react";
 import { toDate } from "utils/date-util";
 import { NewsRegistrationDTO } from "utils/types/response-types";
 import NewsCard from "news/NewsCard";
+import { PlusIcon } from "@navikt/aksel-icons";
+import styles from "./News.module.scss";
 
 const News = () => {
   const [newsStatus, setNewsStatus] = useState("ACTIVE");
@@ -50,16 +52,18 @@ const News = () => {
               <ToggleGroup.Item value={"EXPIRED"}>Utgåtte</ToggleGroup.Item>
             </ToggleGroup>
 
-            {/*<Button
-              className={styles.createNewsButton}
-              variant="secondary"
-              size="medium"
-              icon={<PlusIcon aria-hidden />}
-              iconPosition="left"
-              onClick={() => navigate("/nyheter/opprett")}
-            >
-              Opprett ny nyhetsmelding
-            </Button>*/}
+            {
+              <Button
+                className={styles.createNewsButton}
+                variant="secondary"
+                size="medium"
+                icon={<PlusIcon aria-hidden />}
+                iconPosition="left"
+                onClick={() => navigate("/nyheter/opprett")}
+              >
+                Opprett ny nyhetsmelding
+              </Button>
+            }
           </HStack>
         </div>
         {isLoadingFilteredResults && <Loader size="3xlarge" />}
