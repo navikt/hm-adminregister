40d7e1857d994fbc687626ebd8dfdc0b3b83695e
Birtheec <Birthe.e.christiansen@gmail.com>
2024-07-15 09:37:49 +0200

    put the images inside imageCard

--- client/src/produkter/tabs/SortingArea.tsx
+++ client/src/produkter/tabs/SortingArea.tsx
@@ -1,68 +1,29 @@
 import SortableList, { SortableItem, SortableKnob } from "react-easy-sort";
 import arrayMove from "array-move";
-import React, { useState } from "react";
+import React from "react";
 import { MediaInfoDTO } from "utils/types/response-types";
-import classNames from "classnames";
-import { smallImageLoader } from "utils/image-util";
+import styles from "./sortingArea.modules.scss";
+import { ImageCard } from "felleskomponenter/ImageCard";
 
 interface Props {
   sortedImages: MediaInfoDTO[];
+  handleDeleteFile: (uri: string) => void;
 }
 
-export default function SortingArea({ sortedImages }: Props) {
+export default function SortingArea({ sortedImages, handleDeleteFile }: Props) {
   const [images, setImages] = React.useState(sortedImages);
 
   const onSortEnd = (oldIndex: number, newIndex: number) => {
     setImages((array) => arrayMove(array, oldIndex, newIndex));
   };
 
-  const [imageLoadingError, setImageLoadingError] = useState(false);
-
-  const [imageModalIsOpen, setImageModalIsOpen] = useState<boolean>(false);
-
   return (
-    <SortableList onSortEnd={onSortEnd} className="list" draggedItemClassName="dragged">
+    <SortableList onSortEnd={onSortEnd} draggedItemClassName="dragged">
       {images.map((image, index) => (
         <SortableItem key={index}>
-          <div className="item">
-            <SortableKnob>
-              <div className={classNames("image-container")}>
-                <button type="button" className="button-image" onClick={() => setImageModalIsOpen(true)}>
-                  {imageLoadingError || !image.uri ? (
-                    <img
-                      src={"/adminregister/assets/image-error.png"}
-                      alt="Produktbilde"
-                      style={{
-                        position: "absolute",
-                        height: "100%",
-                        width: "100%",
-                        inset: "0px",
-                        objectFit: "contain",
-                        color: "transparent",
-                        padding: "10px",
-                      }}
-                      sizes="50vw"
-                    />
-                  ) : (
-                    <img
-                      src={smallImageLoader({ src: image.uri, width: 400 })}
-                      alt={"OBS mangler alt-tekst"}
-                      style={{
-                        position: "absolute",
-                        height: "100%",
-                        width: "100%",
-                        inset: "0px",
-                        objectFit: "contain",
-                        color: "transparent",
-                        padding: "10px",
-                      }}
-                      sizes="50vw"
-                    />
-                  )}
-                </button>
-              </div>
-            </SortableKnob>
-          </div>
+          <SortableKnob>
+            <ImageCard mediaInfo={image} handleDeleteFile={handleDeleteFile} showMenuButton={true} />
+          </SortableKnob>
         </SortableItem>
       ))}
     </SortableList>
