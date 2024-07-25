5e60e426a882bf39a275ad28a95943c3d4efea8c
Atle Nordland <atle.nordland@nav.no>
2024-07-25 12:05:43 +0200

    fjern hack som ikke trengs lenger

--- client/vitest-setup.ts
+++ client/vitest-setup.ts
@@ -7,8 +7,6 @@ import { toHaveNoViolations } from "jest-axe";
 expect.extend(matchers);
 expect.extend(toHaveNoViolations);
 
-HTMLCanvasElement.prototype.getContext = vi.fn();
-
 vi.mock("environments", () => ({
   HM_REGISTER_URL: vi.fn(() => "http://localhost:8080"),
   VITE_HM_REGISTER_URL: vi.fn(() => "http://localhost:8082/imageproxy"),
