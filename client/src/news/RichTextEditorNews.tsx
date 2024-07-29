b93f26cc10e93c8f2911b3b79c803b63df643438
albert bayazidi <albert.bayazidi@gmail.com>
2024-07-29 08:52:07 +0200

    added custom class to all ol and ul element

--- client/src/news/RichTextEditorNews.tsx
+++ client/src/news/RichTextEditorNews.tsx
@@ -16,6 +16,42 @@ export const NewEditor = forwardRef<Quill | null, ThirdProps>(
   function TempComp({ onTextChange, defaultValue, className }: ThirdProps, ref) {
     const containerRef = useRef<HTMLDivElement>(null);
     const onTextChangeRef = useRef(onTextChange);
+
+    const checkContentForList = (html:string) : string  =>{
+      const regExOl = /<ol>/g
+      const regExUl = /<ul>/g
+      const olMatch = [...html.matchAll(regExOl)]
+      const ulMatch = [...html.matchAll(regExUl)]
+      
+      if (olMatch.length && ulMatch.length)
+        return "both"
+      else if (olMatch.length)
+        return "ol"
+      else if (ulMatch.length)
+        return "ul"
+      else return "nothing"
+    }
+    
+    const handleQuery = (quill:Quill, state:string) => {
+      if (state == "both"){
+        quill.root.querySelectorAll('ol').forEach((element) => {
+        element.classList.add('quill-ol');
+      });
+      quill.root.querySelectorAll('ul').forEach((element) => {
+        element.classList.add('quill-ul');
+      })}  
+      else if (state == "ol"){
+        quill.root.querySelectorAll('ol').forEach((element) => {
+          element.classList.add('quill-ol');
+        });
+        }
+      else if (state == "ul"){
+        quill.root.querySelectorAll('ul').forEach((element) => {
+          element.classList.add('quill-ul');
+        });
+      }
+    }
+
     useLayoutEffect(() => {
       onTextChangeRef.current = onTextChange;
     });
@@ -39,54 +75,6 @@ export const NewEditor = forwardRef<Quill | null, ThirdProps>(
       });
       delete quill.getModule("keyboard").bindings["9"]
 
-      const applyQuillClass = () => {
-        // WHOLE TEXT
-        const content = quill.root.innerHTML;
-        console.log("raw",content)
-
-        const contentRegex = /(?<=>)[a-zA-Z]*?[^ ](?=<)|<br>/g //ALSO GETS BR might not be good
-        const quillClass = "className=quillTag>" 
-        
-        const tagRegex  = /<[a-z]*[^br]>/g
-        const endTagRegex = /<\/[a-z]*>/g
-        const ListRegex = /<.l>.*?<\/.l>/g
-
-        const contentOflists = [...content.matchAll(ListRegex)];
-
-        const contentArray = [...content.matchAll(contentRegex)];
-        const tagArrayWithoutBr = [...content.matchAll(tagRegex)];
-
-        const endTagArray = [...content.matchAll(endTagRegex)];
-        console.log("tags",tagArrayWithoutBr)
-        console.log("contet",contentArray)
-        console.log("endtags",endTagArray)
-
-        var currUlOl = 0
-         for (let i = 0; i < tagArrayWithoutBr.length; i++){
-          console.log(i)
-          console.log(tagArrayWithoutBr[i][0])
-
-          if (tagArrayWithoutBr[i][0] === "<ol>" || tagArrayWithoutBr[i][0] === "<ul>" ){
-            //HERE WE PICK OUT THE CURRENT OL/UL
-            const listContent = contentOflists[currUlOl][0]; 
-            const listItems = [...listContent.matchAll(/<li>.*?<\/li>/g)];
-            //HERE WE PRINT OUT THE VALUES OF EACH LI
-            for (let j = 0; j < listItems.length; j++){
-              console.log(listItems[j][0])
-            }
-            currUlOl += 1
-            //JUMP FURTHER INTO TagArray since we have printed out mutiple
-            i = i + listItems.length
-            console.log(endTagArray[i][0])
-          }
-          else{
-            console.log(contentArray[i][0]) // CRASHES HERE
-            console.log(endTagArray[i][0])
-          }
-        } 
-      };
-
-
       if (ref && typeof ref === "function") {
         ref(quill);
       } else if (ref && "current" in ref) {
@@ -99,8 +87,8 @@ export const NewEditor = forwardRef<Quill | null, ThirdProps>(
       }
 
       quill.on("text-change", () => {
-
-        applyQuillClass()
+        const state = checkContentForList(quill.root.innerHTML)
+        handleQuery(quill,state)
         onTextChange(quill.root.innerHTML)
       });
 
