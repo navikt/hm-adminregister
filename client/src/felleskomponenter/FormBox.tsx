e493b003aaa09bb8208a2096bf42299f8cd79b8c
Torkild <torkild.engen.finne@nav.no>
2024-07-30 16:13:32 +0200

    added new box component to use on forms

---
+++ client/src/felleskomponenter/FormBox.tsx
@@ -0,0 +1,33 @@
+import { Buldings3Icon } from "@navikt/aksel-icons"
+import { Box, Heading, VStack } from "@navikt/ds-react"
+import { ReactNode } from "react";
+import "./form-box.scss";
+
+export interface FormBoxProps {
+    children?: ReactNode | undefined;
+    title: string
+}
+export default function FormBox(props: FormBoxProps) {
+
+    return (
+        <div className="create-new-supplier">
+            <Box
+                background="surface-default"
+                padding="8"
+                paddingInline="20"
+                borderRadius="large"
+                shadow="medium"
+            >
+                <VStack align="center" gap="8">
+                    <VStack align="center" gap="4">
+                        <Buldings3Icon title="a11y-title" width={43} height={43} aria-hidden />
+                        <Heading level="1" size="large" align="center">
+                            {props.title}
+                        </Heading>
+                    </VStack>
+                    {props.children}
+                </VStack>
+            </Box >
+        </div >
+    )
+}
