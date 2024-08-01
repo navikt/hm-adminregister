b8defc7207ac535d5e9310092ac86a40c85cf57a
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-08-01 13:26:51 +0200

    Fix of import katalogfil

--- client/src/agreements/import/valideringsside/ProductAgreementVariants.tsx
+++ client/src/agreements/import/valideringsside/ProductAgreementVariants.tsx
@@ -1,4 +1,4 @@
-import { ExpansionCard } from "@navikt/ds-react";
+import { ExpansionCard, HStack, Tag } from "@navikt/ds-react";
 import { ProductAgreementsTable } from "agreements/import/valideringsside/ProductAgreementsTable";
 import React from "react";
 import { ProductAgreementRegistrationDTO } from "utils/types/response-types";
@@ -6,15 +6,22 @@ import { ProductAgreementRegistrationDTO } from "utils/types/response-types";
 interface Props {
   seriesUuid?: string;
   title?: string;
+  accessory: boolean;
+  sparePart: boolean;
+  newProduct: boolean;
   variants: ProductAgreementRegistrationDTO[];
 }
 
-export const ProductAgreementVariants = ({ seriesUuid, title, variants }: Props) => {
+export const ProductAgreementVariants = ({ seriesUuid, title, variants, accessory, sparePart, newProduct }: Props) => {
   return (
     <ExpansionCard key={seriesUuid} aria-label="Produktserie med varianter" style={{ width: "90vw" }} size="small">
       <ExpansionCard.Header>
         <ExpansionCard.Title>
-          {title} ({variants.length})
+          <HStack gap="2">
+            {title} ({variants.length})
+            {newProduct ? <Tag variant="success">Nytt produkt</Tag> : <Tag variant="alt2">Eksisterende produkt</Tag>}
+            <Tag variant={"info"}>{accessory ? "Tilbehør" : sparePart ? "Reservedel" : "Hovedprodukt"}</Tag>
+          </HStack>
         </ExpansionCard.Title>
       </ExpansionCard.Header>
       <ExpansionCard.Content>
