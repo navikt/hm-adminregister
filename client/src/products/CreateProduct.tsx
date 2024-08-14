bf3f1b52ea10582ae73011d1123e6f41fc033f95
marte pedersen <marte.pedersen@nav.no>
2024-08-14 12:36:13 +0200

    ta ibruk iso-combobox i stedet for combobox og fikse opp i buggy funksjoner

--- client/src/products/CreateProduct.tsx
+++ client/src/products/CreateProduct.tsx
@@ -1,88 +1,121 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, HStack, Heading, TextField, VStack } from "@navikt/ds-react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { Button, HStack, TextField, VStack } from "@navikt/ds-react";
 import "./create-product.scss";
 import { useErrorStore } from "utils/store/useErrorStore";
 import { useIsoCategories } from "utils/swr-hooks";
 import { useNavigate } from "react-router-dom";
 import { SeriesDraftWithDTO } from "utils/types/response-types";
 import { labelRequired } from "utils/string-util";
-import Combobox from "felleskomponenter/Combobox";
-import { createNewSeriesSchema } from "utils/zodSchema/newSeries";
 import { draftNewSeries } from "api/SeriesApi";
 import FormBox from "felleskomponenter/FormBox";
 import { PackageIcon } from "@navikt/aksel-icons";
+import { useState } from "react";
+import { ISO_Combobox } from "products/iso-combobox";
 
-type FormData = z.infer<typeof createNewSeriesSchema>;
+type Error = {
+  titleField?: boolean | undefined;
+  isoField?: boolean | undefined;
+  message: string | undefined;
+};
 
 export default function CreateProduct() {
   const { setGlobalError } = useErrorStore();
-  const { isoCategories, isoError } = useIsoCategories();
+  const { isoCategories } = useIsoCategories();
   const navigate = useNavigate();
-  const {
-    handleSubmit,
-    register,
-    formState: { errors, isSubmitting },
-    setValue,
-  } = useForm<FormData>({
-    resolver: zodResolver(createNewSeriesSchema),
-    mode: "onSubmit",
-  });
-
-  async function onSubmit(data: FormData) {
-    const newSeries: SeriesDraftWithDTO = {
-      title: data.productName,
-      isoCategory: data.isoCategory,
-    };
-
-    draftNewSeries(newSeries)
-      .then((newSeries) => {
-        navigate(`/produkter/${newSeries.id}`);
-      })
-      .catch((error) => {
-        setGlobalError(error);
-      });
-  }
-
+  const [title, setTitle] = useState<string>("");
+  const [isoCategory, setIsoCategory] = useState<string>("");
+  const [error, setError] = useState<Error | undefined>(undefined);
   const uniqueIsoCodes = isoCategories?.filter((cat) => cat.isoCode && cat.isoCode.length >= 8);
   const isoCodesAndTitles = uniqueIsoCodes?.map((cat) => cat.isoTitle + " - " + cat.isoCode).sort();
 
+  const validateFields = () => {
+    setError(undefined);
+    if (!title || title === "") {
+      setError({ titleField: true, message: "Du må skrive en tittel" });
+      return false;
+    }
+    if (!isoCategory || isoCategory === "") {
+      setError({ isoField: true, message: "Du må velge en iso-kategori" });
+      return false;
+    }
+    return true;
+  };
+
+  async function onSubmit() {
+    if (validateFields()) {
+      const newSeries: SeriesDraftWithDTO = {
+        title: title,
+        isoCategory: handleSetFormValueIso(isoCategory),
+      };
+
+      draftNewSeries(newSeries)
+        .then((newSeries) => {
+          navigate(`/produkter/${newSeries.id}`);
+        })
+        .catch((error) => {
+          setGlobalError(error);
+        });
+    }
+  }
+
   const handleSetFormValueIso = (value: string) => {
     const parts = value.split("-");
-    const lastPartWithoutSpaces = parts[1].replace(/\s/g, ""); // Remove spaces
-    setValue("isoCategory", lastPartWithoutSpaces);
+    return parts[1].replace(/\s/g, ""); // Remove spaces
+  };
+
+  const [inputValue, setInputValue] = useState("");
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+  const [filteredOptions, setFilteredOptions] = useState<string[]>();
+
+  const onToggleSelected = (option: string, isSelected: boolean) => {
+    if (isSelected) {
+      setSelectedOptions([option]);
+      setIsoCategory(option);
+    } else {
+      setSelectedOptions([]);
+      setIsoCategory("");
+    }
+  };
+  const filter = () => {
+    const filtered = isoCodesAndTitles?.filter((option) => option.toLowerCase().includes(inputValue.toLowerCase()));
+    setFilteredOptions(filtered);
   };
 
   return (
     <FormBox title="Kom i gang med nytt produkt" icon={<PackageIcon />}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <VStack gap="7">
-          <TextField
-            {...register("productName", { required: true })}
-            label={labelRequired("Produktnavn")}
-            id="productName"
-            name="productName"
-            type="text"
-            error={errors?.productName?.message}
-          />
-          <Combobox
-            {...register("isoCategory", { required: true })}
-            options={isoCodesAndTitles}
-            setValue={handleSetFormValueIso}
-            label={labelRequired("Iso-kategori (kode)")}
-            errorMessage={errors?.isoCategory && "Du må velge en isokategori"}
-          />
-          <HStack gap="4">
-            <Button type="reset" variant="secondary" size="medium" onClick={() => window.history.back()}>
-              Avbryt
-            </Button>
-            <Button type="submit" size="medium" disabled={isSubmitting}>
-              Opprett
-            </Button>
-          </HStack>
-        </VStack>
-      </form>
+      <VStack gap="7">
+        <TextField
+          label={labelRequired("Produktnavn")}
+          id="productName"
+          name="productName"
+          type="text"
+          onChange={(event) => setTitle(event.target.value)}
+          onBlur={() => setError(undefined)}
+          onFocus={() => setError(undefined)}
+          error={error?.titleField ? error.message : ""}
+        />
+        <ISO_Combobox
+          label={labelRequired("Iso-kategori (kode)")}
+          description={"Søk etter isokategori produktet passer best inn i"}
+          selectedOptions={selectedOptions}
+          onChange={(value) => {
+            setInputValue(value || "");
+            filter();
+          }}
+          filteredOptions={filteredOptions}
+          options={isoCodesAndTitles || []}
+          onToggleSelected={onToggleSelected}
+          error={error?.isoField ? error.message : ""}
+          maxSelected={{ limit: 1 }}
+        />
+        <HStack gap="4">
+          <Button type="reset" variant="secondary" size="medium" onClick={() => window.history.back()}>
+            Avbryt
+          </Button>
+          <Button type="submit" size="medium" onClick={onSubmit}>
+            Opprett
+          </Button>
+        </HStack>
+      </VStack>
     </FormBox>
   );
 }
