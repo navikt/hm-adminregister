c6c2dae611d92b8bd4cc7994d22375fcddc13cbe
Arshad A. Kamran <arshad.ali.kamran@nav.no>
2024-10-11 09:30:57 +0200

    Don't show navigation if just one image

--- client/src/felleskomponenter/ImageModal.tsx
+++ client/src/felleskomponenter/ImageModal.tsx
@@ -1,18 +1,18 @@
 import {Button, Modal} from "@navikt/ds-react";
-import { MediaInfoDTO } from "utils/types/response-types";
-import { mediumImageLoader } from "utils/image-util";
+import {MediaInfoDTO} from "utils/types/response-types";
+import {mediumImageLoader} from "utils/image-util";
 import styles from "./ImageModal.module.scss";
 import {ChevronLeftIcon, ChevronRightIcon} from "@navikt/aksel-icons";
 import {useState} from "react";
 
 interface Props {
-  mediaInfo: MediaInfoDTO[];
-  index: number;
-  onClose: () => void;
-  isModalOpen: boolean;
+    mediaInfo: MediaInfoDTO[];
+    index: number;
+    onClose: () => void;
+    isModalOpen: boolean;
 }
 
-const ImageModal = ({ mediaInfo, index, onClose, isModalOpen }: Props) => {
+const ImageModal = ({mediaInfo, index, onClose, isModalOpen}: Props) => {
 
     const [newIndex, setNewIndex] = useState(index);
     const onCloseResetImageBrowsing = () => {
@@ -29,39 +29,43 @@ const ImageModal = ({ mediaInfo, index, onClose, isModalOpen }: Props) => {
     };
 
     return (
-    <Modal
-      className={styles.imageModal}
-      open={isModalOpen}
-      header={{
-        heading: "",
-        closeButton: true,
-      }}
-      onClose={onCloseResetImageBrowsing}
-    >
-      <Modal.Body className={styles.imageModalModalBody}>
-
-          <Button
-              variant="tertiary"
-              icon={<ChevronLeftIcon fontSize="3rem" aria-hidden />}
-              className={"leftButton"}
-              onClick={handleMoveLeft}
-              title="Flytt til venstre"
-          ></Button>
-        <img
-          src={mediumImageLoader({ src: mediaInfo[newIndex].uri, width: 800 })}
-          onError={() => {}}
-          alt={mediaInfo[newIndex].text ?? "OBS mangler alt-tekst"}
-        />
-          <Button
-              variant="tertiary"
-              icon={<ChevronRightIcon fontSize="3rem" aria-hidden />}
-              className={"rightButton"}
-              onClick={handleMoveRight}
-              title="Flytt til høyre"
-          ></Button>
-      </Modal.Body>
-    </Modal>
-  );
+        <Modal
+            className={styles.imageModal}
+            open={isModalOpen}
+            header={{
+                heading: "",
+                closeButton: true,
+            }}
+            onClose={onCloseResetImageBrowsing}
+        >
+            <Modal.Body className={styles.imageModalModalBody}>
+                {(mediaInfo.length > 1) && (
+                    <Button
+                        variant="tertiary"
+                        icon={<ChevronLeftIcon fontSize="3rem" aria-hidden/>}
+                        className={"leftButton"}
+                        onClick={handleMoveLeft}
+                        title="Flytt til venstre"
+                    ></Button>
+                )}
+                <img
+                    src={mediumImageLoader({src: mediaInfo[newIndex].uri, width: 800})}
+                    onError={() => {
+                    }}
+                    alt={mediaInfo[newIndex].text ?? "OBS mangler alt-tekst"}
+                />
+                {(mediaInfo.length > 1) && (
+                    <Button
+                        variant="tertiary"
+                        icon={<ChevronRightIcon fontSize="3rem" aria-hidden/>}
+                        className={"rightButton"}
+                        onClick={handleMoveRight}
+                        title="Flytt til høyre"
+                    ></Button>
+                )}
+            </Modal.Body>
+        </Modal>
+    );
 };
 
 export default ImageModal;
