9ed280b2a1e0689748d5f9f588b7464d7677c8b5
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-10-18 13:06:23 +0200

    Remove console logs

--- client/src/mocks/initMsw.ts
+++ client/src/mocks/initMsw.ts
@@ -1,8 +1,6 @@
 import { baseUrl } from "utils/swr-hooks";
 
 export async function initMsw(): Promise<unknown> {
-  console.log(window.appSettings.MILJO);
-  console.log(window.appSettings.USE_MSW);
   if (!window.appSettings.USE_MSW) {
     return;
   }
