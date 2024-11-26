49eb6fa4aca5fb6ec4143f901626e0540b1333b6
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-11-26 15:20:55 +0100

    Link to main page

--- client/src/users/reset-password/PasswordResetReceit.tsx
+++ client/src/users/reset-password/PasswordResetReceit.tsx
@@ -1,12 +1,13 @@
 import { BodyShort, VStack } from "@navikt/ds-react";
 import FormBox from "felleskomponenter/FormBox";
+import { baseUrl } from "utils/swr-hooks";
 
 export const PasswordResetReceit = () => {
   return (
     <FormBox title="Passordet er tilbakestilt.">
       <VStack gap="7" style={{ width: "300px" }}>
         <BodyShort>
-          Du kan nå <a href={"/"}> logge inn med ditt nye passord.</a>
+          Du kan nå <a href={baseUrl("/")}> logge inn med ditt nye passord.</a>
         </BodyShort>
       </VStack>
     </FormBox>
