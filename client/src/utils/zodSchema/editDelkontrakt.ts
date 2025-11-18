31a73fa053a2dfad8d5222ff9c599c8c24434cc1
Aasmund Nordstoga <anostoga@gmail.com>
2025-11-18 11:16:08 +0100

    Allow postpending of a letter after delkontrakt number

--- client/src/utils/zodSchema/editDelkontrakt.ts
+++ client/src/utils/zodSchema/editDelkontrakt.ts
@@ -1,7 +1,13 @@
 import z from "zod";
 
 export const editDelkontraktSchema = z.object({
-  tittel: z.string().min(1, { message: "Tittel på delkontrakt kan ikke være tom" }),
+  tittel: z
+    .string()
+    .min(1, { message: "Tittel på delkontrakt kan ikke være tom" })
+    .regex(/^[0-9]{1,2}[A-Za-z]?[:.].*/, {
+      message:
+        "Tittel må starte med et tall, eventuelt én bokstav, etterfulgt av : eller .",
+    }),
   beskrivelse: z.string().min(1, { message: "Beskrivelse kan ikke være tom" }),
 });
 
--- client/src/utils/zodSchema/newDelkontrakt.ts
+++ client/src/utils/zodSchema/newDelkontrakt.ts
@@ -4,6 +4,11 @@ export const createNewDelkontraktSchema = z.object({
   tittel: z
     .string()
     .min(1, { message: "Tittel på delkontrakt kan ikke være tom" })
-    .regex(/^[0-9]{1,2}(-[0-9]{1,2})?[:.].*/, { message: "Tittel må starte med et tall eller tallgruppe etterfulgt av : eller ." }),
-  beskrivelse: z.string().min(1, { message: "Beskrivelse kan ikke være tom" }),
+    .regex(/^[0-9]{1,2}[A-Za-z]?[:.].*/, {
+      message:
+        "Tittel må starte med et tall, eventuelt én bokstav, etterfulgt av : eller .",
+    }),
+  beskrivelse: z
+    .string()
+    .min(1, { message: "Beskrivelse kan ikke være tom" }),
 });
