5156fc388471dd6ce6092d58565ba16300a758d8
marte pedersen <marte.pedersen@nav.no>
2026-05-22 17:11:47 +0200

    design, bytte til tabell

--- client/src/techlabels/TechLabels.tsx
+++ client/src/techlabels/TechLabels.tsx
@@ -15,7 +15,9 @@ import {
   Loader,
   Modal,
   Pagination,
+  Popover,
   Search,
+  Table,
   Tooltip,
   VStack,
 } from '@navikt/ds-react'
@@ -94,6 +96,23 @@ export const TechLabels = () => {
 
   const paginatedLabels = filteredLabels.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE)
 
+  const optionsFix = (options: string[]) => {
+    const allOptions = options.join(', ')
+    return options && options.length > 0 ? (
+      <Tooltip content={allOptions}>
+        {options.length > MAX_VISIBLE_OPTIONS ? (
+          <BodyShort>
+            {options.slice(0, MAX_VISIBLE_OPTIONS).join(', ')}, ... ({options.length})
+          </BodyShort>
+        ) : (
+          <BodyShort className={styles.column}>{allOptions}</BodyShort>
+        )}
+      </Tooltip>
+    ) : (
+      <BodyShort>-</BodyShort>
+    )
+  }
+
   return (
     <main className="show-menu">
       <DeleteTechDataModal
@@ -110,126 +129,96 @@ export const TechLabels = () => {
           Teknisk-data beskrivelser
         </Heading>
 
-        <div className={styles.techLabelsContainer}>
-          <VStack gap="space-24">
-            <HStack justify="space-between" wrap gap="space-16" marginBlock="space-24 space-0">
-              <Box role="search" className="search-box">
-                <Search
-                  label="Søk etter navn på teknisk-data"
-                  variant="simple"
-                  placeholder="Søk etter navn på teknisk-data"
-                  size="medium"
-                  value={searchTerm}
-                  onChange={setSearchTerm}
-                />
-              </Box>
-              <Box role="search" className="search-box">
-                <Search
-                  label="Søk etter ISO-kode"
-                  variant="simple"
-                  placeholder="Søk etter ISO-kode"
-                  size="medium"
-                  value={searchIsoCode}
-                  onChange={updateUrlOnSearchIsoCodeChange}
-                />
-              </Box>
-
-              <Button
-                variant="secondary"
+        <VStack gap="space-24" className={styles.techLabelsContainer}>
+          <HStack justify="space-between" wrap gap="space-16" marginBlock="space-24 space-0">
+            <Box role="search" className="search-box">
+              <Search
+                label="Søk etter navn på teknisk-data"
+                variant="simple"
+                placeholder="Søk etter navn på teknisk-data"
                 size="medium"
-                icon={<PlusIcon aria-hidden />}
-                iconPosition="left"
-                onClick={() => navigate('/tekniskdata/opprett')}
-              >
-                Opprett ny teknisk-data beskrivelse
-              </Button>
-            </HStack>
+                value={searchTerm}
+                onChange={setSearchTerm}
+              />
+            </Box>
+            <Box role="search" className="search-box">
+              <Search
+                label="Søk etter ISO-kode"
+                variant="simple"
+                placeholder="Søk etter ISO-kode"
+                size="medium"
+                value={searchIsoCode}
+                onChange={updateUrlOnSearchIsoCodeChange}
+              />
+            </Box>
 
-            <div className="panel-list__container">
-              <div className={styles.cardRow + ' ' + styles.cardHeader}>
-                <BodyShort className={`${styles.cardValue} ${styles.mediumColumn}`}>
-                  <strong>Navn</strong>
-                </BodyShort>
-                <BodyShort className={`${styles.cardValue} ${styles.shortColumn}`}>
-                  <strong>ISO-kode</strong>
-                </BodyShort>
-                <BodyShort className={`${styles.cardValue} ${styles.shortColumn}`}>
-                  <strong>Type</strong>
-                </BodyShort>
-                <BodyShort className={`${styles.cardValue} ${styles.shortColumn}`}>
-                  <strong>Enhet</strong>
-                </BodyShort>
-                <BodyShort className={`${styles.cardValue} ${styles.shortColumn}`}>
-                  <strong>Sortering</strong>
-                </BodyShort>
-                <BodyShort className={`${styles.cardValue} ${styles.mediumColumn}`}>
-                  <strong>Obligatorisk</strong>
-                </BodyShort>
-                <BodyShort className={`${styles.cardValue} ${styles.optionsColumn}`}>
-                  <strong>Alternativer</strong>
-                </BodyShort>
-                <span className={styles.editButtonHeader}></span>
-              </div>
+            <Button
+              variant="secondary"
+              size="medium"
+              icon={<PlusIcon aria-hidden />}
+              iconPosition="left"
+              onClick={() => navigate('/tekniskdata/opprett')}
+            >
+              Opprett ny teknisk-data beskrivelse
+            </Button>
+          </HStack>
 
-              {paginatedLabels.map((label) => (
-                <Box key={label.id} className={styles.cardBox}>
-                  <div className={styles.cardRow}>
-                    <BodyShort className={`${styles.cardValue} ${styles.mediumColumn}`}>{label.label}</BodyShort>
-                    <BodyShort className={`${styles.cardValue} ${styles.shortColumn}`}>{label.isoCode}</BodyShort>
-                    <BodyShort className={`${styles.cardValue} ${styles.shortColumn}`}>{label.type}</BodyShort>
-                    <BodyShort className={`${styles.cardValue} ${styles.shortColumn}`}>{label.unit}</BodyShort>
-                    <BodyShort className={`${styles.cardValue} ${styles.shortColumn}`}>{label.sort}</BodyShort>
-                    <BodyShort className={`${styles.cardValue} ${styles.mediumColumn}`}>
-                      {label.required ? 'Ja' : 'Nei'}
-                    </BodyShort>
-                    <BodyShort className={`${styles.cardValue} ${styles.optionsColumn}`}>
-                      {label.options && label.options.length > 0 ? (
-                        label.options.length > MAX_VISIBLE_OPTIONS ? (
-                          <Tooltip content={label.options.join(', ')}>
-                            <span>
-                              {label.options.slice(0, MAX_VISIBLE_OPTIONS).join(', ')}, ... ({label.options.length})
-                            </span>
-                          </Tooltip>
-                        ) : (
-                          label.options.join(', ')
-                        )
-                      ) : (
-                        '-'
-                      )}
-                    </BodyShort>
-                    <span className={styles.editButton}>
-                      <Button
-                        size="xsmall"
-                        variant="tertiary"
-                        icon={<PencilWritingIcon aria-hidden />}
-                        onClick={() => navigate(`/tekniskdata/rediger/${label.id}`, { state: label })}
-                        aria-label="Rediger"
-                      />
-                      <Button
-                        size="xsmall"
-                        variant="tertiary"
-                        icon={<TrashIcon aria-hidden />}
-                        onClick={() => handleDelete(label.id, false)}
-                        aria-label="Slett"
-                        style={{ marginLeft: '0.5rem' }}
-                      />
-                    </span>
-                  </div>
-                </Box>
+          <Table size={'small'}>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell className={styles.mediumColumn}>Navn</Table.HeaderCell>
+                <Table.HeaderCell className={styles.shortColumn}>ISO-kode</Table.HeaderCell>
+                <Table.HeaderCell className={styles.shortColumn}>Type</Table.HeaderCell>
+                <Table.HeaderCell className={styles.shortColumn}>Enhet</Table.HeaderCell>
+                <Table.HeaderCell className={styles.shortColumn}>Sortering</Table.HeaderCell>
+                <Table.HeaderCell className={styles.requiredColumn}>Obligatorisk</Table.HeaderCell>
+                <Table.HeaderCell className={styles.optionsColumn}>Alternativer</Table.HeaderCell>
+                <Table.HeaderCell className={styles.editButtonHeader} />
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {paginatedLabels.map((label, index) => (
+                <Table.Row key={index + label.id}>
+                  <Table.DataCell className={styles.mediumColumn}>{label.label}</Table.DataCell>
+                  <Table.DataCell className={styles.shortColumn}>{label.isoCode}</Table.DataCell>
+                  <Table.DataCell className={styles.shortColumn}>{label.type}</Table.DataCell>
+                  <Table.DataCell className={styles.shortColumn}>{label.unit}</Table.DataCell>
+                  <Table.DataCell className={styles.shortColumn}>{label.sort}</Table.DataCell>
+                  <Table.DataCell className={styles.requiredColumn}>{label.required ? 'Ja' : 'Nei'}</Table.DataCell>
+                  <Table.DataCell className={styles.optionsColumn}>{optionsFix(label.options)}</Table.DataCell>
+                  <Table.DataCell className={styles.editButton}>
+                    <Button
+                      size="xsmall"
+                      variant="tertiary"
+                      icon={<PencilWritingIcon aria-hidden />}
+                      onClick={() => navigate(`/tekniskdata/rediger/${label.id}`, { state: label })}
+                      aria-label="Rediger"
+                    />
+                    <Button
+                      size="xsmall"
+                      variant="tertiary"
+                      icon={<TrashIcon aria-hidden />}
+                      onClick={() => handleDelete(label.id, false)}
+                      aria-label="Slett"
+                      style={{ marginLeft: '0.5rem' }}
+                    />
+                  </Table.DataCell>
+                </Table.Row>
               ))}
-              {filteredLabels.length > PAGE_SIZE && (
-                <Pagination
-                  page={page}
-                  onPageChange={setPage}
-                  count={Math.ceil(filteredLabels.length / PAGE_SIZE)}
-                  boundaryCount={1}
-                  siblingCount={0}
-                  size="small"
-                />
-              )}
-            </div>
-          </VStack>
-        </div>
+            </Table.Body>
+          </Table>
+
+          {filteredLabels.length > PAGE_SIZE && (
+            <Pagination
+              page={page}
+              onPageChange={setPage}
+              count={Math.ceil(filteredLabels.length / PAGE_SIZE)}
+              boundaryCount={1}
+              siblingCount={0}
+              size="small"
+            />
+          )}
+        </VStack>
       </div>
     </main>
   )
