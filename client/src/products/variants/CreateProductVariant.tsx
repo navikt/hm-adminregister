fd31de15322fcfc5d91c09e6d7d1773dff4b154f
Arshad A. Kamran <arshad.ali.kamran@nav.no>
2026-07-27 12:53:00 +0200

    iht forb. 8 - krav el. hjelpetekst for bedre variantnavn

--- client/src/products/variants/CreateProductVariant.tsx
+++ client/src/products/variants/CreateProductVariant.tsx
@@ -12,8 +12,8 @@ import { newProductVariantSchema } from 'utils/zodSchema/newProduct'
 import { z } from 'zod'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { LayersIcon } from '@navikt/aksel-icons'
-import { Button, HStack, TextField, VStack } from '@navikt/ds-react'
+import { CheckmarkCircleIcon, LayersIcon, XMarkOctagonIcon } from '@navikt/aksel-icons'
+import { Alert, BodyLong, Button, Heading, HStack, List, ReadMore, TextField, VStack } from '@navikt/ds-react'
 
 type FormData = z.infer<typeof newProductVariantSchema>
 
@@ -67,6 +67,7 @@ const CreateProductVariant = () => {
     <FormBox title="Legg til variant" icon={<LayersIcon />}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <VStack gap="space-16">
+          <VariantNameRequirementBox />
           <TextField
             {...register('articleName', { required: true })}
             label={labelRequired('Variantnavn')}
@@ -98,3 +99,44 @@ const CreateProductVariant = () => {
 }
 
 export default CreateProductVariant
+
+// TODO: Innholdet under er en midlertidig plassholder og må utarbeides i samarbeid med fagpersoner.
+const VariantNameRequirementBox = () => (
+  <Alert variant="info">
+    <Heading size="small" level="2">
+      Tips til gode variantnavn:
+    </Heading>
+    <BodyLong weight="semibold" textColor= "subtle" >
+      (disse er bare forslag og vil bli endret i dialog med IHT)
+    </BodyLong>
+    <BodyLong>- er kort og presist</BodyLong>
+    <BodyLong><em>- unngå å gjenta produktnavnet eller må vi ha produktnavn med slik at man kan kjenne til produktet/serien denne varianten tilhører til?</em></BodyLong>
+    <BodyLong>- beskriver det som skiller varianten fra andre varianter i serien</BodyLong>
+    <BodyLong>- unngå å ha egenskaper som størrelse eller andre mål i variantnavnet.....</BodyLong>
+    <ReadMore header="Se eksempler på gode og dårlige variantnavn">
+      <VStack gap="space-16">
+        <VStack gap="space-4">
+          <HStack gap="space-8" align="center">
+            <CheckmarkCircleIcon aria-hidden fontSize="1.25rem" color="var(--ax-text-success-subtle)" />
+            <BodyLong weight="semibold">Gode eksempler</BodyLong>
+          </HStack>
+          <List>
+            <List.Item>???</List.Item>
+            <List.Item>???</List.Item>
+          </List>
+        </VStack>
+
+        <VStack gap="space-4">
+          <HStack gap="space-8" align="center">
+            <XMarkOctagonIcon aria-hidden fontSize="1.25rem" color="var(--ax-text-danger-subtle)" />
+            <BodyLong weight="semibold">Unngå</BodyLong>
+          </HStack>
+          <List>
+            <List.Item><a href="https://finnhjelpemiddel.nav.no/produkt/fcce027c-af6e-4bbf-8e58-61cb27247e20">Netti III HD 2016</a></List.Item>
+            <List.Item><a href="https://finnhjelpemiddel.nav.no/produkt/eb687083-b549-435f-9628-a08e950e8d10?term=FlexiElectric">FlexiElectric</a></List.Item>
+          </List>
+        </VStack>
+      </VStack>
+    </ReadMore>
+  </Alert>
+)
